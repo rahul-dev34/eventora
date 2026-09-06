acc41187b51f4c253120b883057717f6105cced3
rahul dev <rahulburnwal03@gmail.com>
2026-09-06 18:30:04 +0530

    fix api url

--- Client/src/utils/axios.js
+++ Client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://eventora-backend-pz07.onrender.com',
+  baseURL: 'https://eventora-backend-pz07.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'  // JSON bhej rhe hain
   }
